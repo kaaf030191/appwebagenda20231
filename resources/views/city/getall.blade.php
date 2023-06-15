feb297f66b99db638e330f5eb22958730582583c
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2023-06-15 09:15:08 -0500

    Feat: 14-06-2023 - Integración parcial de AdminLte.
    Incorporación parcial de la plantilla AdminLte.

--- resources/views/city/getall.blade.php
+++ resources/views/city/getall.blade.php
@@ -10,12 +10,14 @@
 		<thead>
 			<tr>
 				<th>Nombre</th>
+				<th>Fecha registro</th>
 			</tr>
 		</thead>
 		<tbody>
 			@foreach($listTCity as $value)
 				<tr>
 					<td style="border: 1px dashed black;">{{$value->name}}</td>
+					<td style="border: 1px dashed black;">{{$value->created_at}}</td>
 				</tr>
 			@endforeach
 		</tbody>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\IndexController;
 use App\Http\Controllers\CityController;
+
+Route::get('/', [IndexController::class, 'actionIndex']);
 
 Route::get('city/getall', [CityController::class, 'actionGetAll']);
